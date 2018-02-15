756ed9f366429339bb79076a74ee774459cb2c64
George Pandzik <gepandz@gmail.com>
2018-02-14 23:12:00 -0800

    Add some comments to clarify things a bit

--- lib/nweaapi.rb
+++ lib/nweaapi.rb
@@ -2,8 +2,11 @@ require "nweaapi/version"
 require 'sinatra'
 require 'datamapper'
 
+# Connect to our local DB, which is kept at lib/resources/blog.db
+# Note: This requires setting up a :default DB at this point
 DataMapper::setup(:default, "sqlite://#{Dir.pwd}/resources/blog.db")
 
+# Define a modle object that DataMapper can use to talk to the DB
 class Post
   include DataMapper::Resource
   property :post_id, Serial
@@ -11,8 +14,10 @@ class Post
   property :body, Text
 end
 
+# We're done defining models, so compile the model so we can use it
 DataMapper.finalize
 
+# Offer some pointers to where to go from here
 get '/' do
   'This is not a valid endpoint. Please use /post or /posts'
 end
