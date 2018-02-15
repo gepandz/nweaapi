7c0638cad4f0ad31de40de84e15383efcd9501e4
George Pandzik <gepandz@gmail.com>
2018-02-14 21:16:59 -0800

    Remove unnecessary test so 'bundle exec rake spec' is happy for the moment

--- spec/nweaapi_spec.rb
+++ spec/nweaapi_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Nweaapi do
   it "has a version number" do
     expect(Nweaapi::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
